f3e9410f4ca28e8e50f6f5c152954c5f4288e309
sai-dd <62632549+sai-dd@users.noreply.github.com>
2020-03-25 12:32:30 -0300

    Add files via upload

---
+++ hjkllç.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define NUM 5
+
+int main(){
+
+    int a, b, c, d, e;
+
+while(e != (3 || b)){
+    printf("Escolha o valor desejado: ");
+scanf("%d", a);
+
+b = 5 * a;
+c = 10 * a;
+d = 1 * a;
+
+printf("\n5 * %d resulta em qual valor:", a);
+printf("\nValor 1 - %d", c);
+printf("\nValor 2 - %d", d);
+printf("\nValor 3 - %d", b);
+
+printf("\n Valor: ");
+scanf("%d", e);
+
+if(e != (3 || b) ){
+    printf("Errado!");
+} else{
+    printf("Correto!");
+}
+
+}
+
+
+
+system("\npause");
+}
